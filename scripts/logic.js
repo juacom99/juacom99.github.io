8774800bc196f9a41ef26bbf5b07c579810464ac
juacom99 <juacom04@gmail.com>
2020-03-11 19:26:40 -0300

    Project list style rethink

--- scripts/logic.js
+++ scripts/logic.js
@@ -54,20 +54,57 @@ function onProjectClick()
                 for (i in data)
                 {
                     repo = data[i];
-                    if (repo.description)
+                   /* if (repo.description)
                     {
                         desc = repo.description
                     } else
                     {
                         desc = "No description for this project"
                     }
-                    pl.append('<div id="' + repo.id + '" class="project"><div class="projectName"><a href="' + repo.html_url + '" target="new">' + repo.name + '</a></div><div class="projectDescription">' + desc + '</div></div>');
-
+                    
+                    pl.append('<div id="' + repo.id + '" class="project">\n\
+                               <div class="projectInfo"> \n\
+                                    <div class="projectName"><a href="' + repo.html_url + '" target="new">' + repo.name + '</a>\n\
+                                    </div><div class="projectDescription">' + desc + '</div>\n\
+                               </div> </div>');*/
+                    
+                    w=GetProjectContent(repo)
+                    console.log(repo)
+                    pl.append(w)
                 }
+
+
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 if (console && console.log) {
                     console.log("La solicitud a fallado: " + textStatus);
                 }
             });
+}
+
+
+function GetProjectContent(repo)
+{
+    if (repo.description)
+    {
+        desc = repo.description
+    } else
+    {
+        desc = "No description for this project"
+    }
+
+
+    ret = '<div id="'+repo.id+'" class="project">\n\
+            <div class="projectInfo">\n\
+                <div class="projectName"><a href="'+repo.html_url+'" target="new">'+repo.name+'</a></div>\n\
+                <div class="projectDescription">'+desc+'</div>\n\
+            </div>\n\
+            <div class="" style="padding-left: 16px;vertical-align: middle;">\n\
+                <span class="programmingLanguageIcon" style="background-color: #ff0000;"></span>\n\
+                <span class="programmingLanguage">'+repo.language+'</span>\n\
+            </div>\n\
+        </div>'
+    
+    return ret;
+
 }
